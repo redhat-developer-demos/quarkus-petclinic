1b19e378bf4a7a40067b48583dedd38575ed2efa
Martin Kouba <mkouba@redhat.com>
2023-01-25 16:41:21 +0100

    Log the exception handled by ErrorExceptionMapper

--- src/main/java/org/quarkus/samples/petclinic/system/ErrorExceptionMapper.java
+++ src/main/java/org/quarkus/samples/petclinic/system/ErrorExceptionMapper.java
@@ -5,15 +5,20 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.jboss.logging.Logger;
+
 @Provider
 public class ErrorExceptionMapper implements ExceptionMapper<Exception> {
+
+    private static final Logger LOG = Logger.getLogger(ErrorExceptionMapper.class);
 
     @Inject
     TemplatesLocale templates;
 
     @Override
     public Response toResponse(Exception exception) {
+        LOG.error("Interal application error", exception);
         return Response.ok(templates.error(exception.getMessage())).build();
     }
-    
+
 }
